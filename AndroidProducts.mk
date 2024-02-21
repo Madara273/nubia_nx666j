89b7ce40c4faa300caa865117518b0d3836e1ac4
Madara273 <156642375+Madara273@users.noreply.github.com>
2024-02-21 11:25:13 +0200

    update

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_NX666J.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_NX666J-user \
+    omni_NX666J-userdebug \
+    omni_NX666J-eng
