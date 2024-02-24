c0738a420b445f3b2edb9812acda9b8a86f41690
Madara273 <156642375+Madara273@users.noreply.github.com>
2024-02-24 05:13:03 +0200

    UPDATE

--- twrp_NX666J.mk
+++ twrp_NX666J.mk
@@ -3,14 +3,9 @@
 # Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
-#
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from NX666J device
 $(call inherit-product, device/nubia/NX666J/device.mk)
